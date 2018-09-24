241171ddcef3c06733d738d9708536e06a4e6ed2
Qwertie64982 <maxwell.a.sherman@live.com>
2018-09-24 16:44:34 -0700

    Separate screen fill into function

--- source/main.c
+++ source/main.c
@@ -1,10 +1,14 @@
 /*
 SPTool by Maxwell Sherman (Qwertie64982)
-Last modified 2018-09-24 3PM PDT
+Last modified 2018-09-24 5PM PDT
 */
 
 #include <3ds.h>
 #include <stdio.h>
+
+enum {red, green, blue, yellow, turquoise, magenta, white, black};
+
+void fillScreen(u8* fb, int color);
 
 int main(int argc, char **argv)
 {
@@ -32,15 +36,7 @@ int main(int argc, char **argv)
     gfxSetDoubleBuffering(GFX_TOP, false);
     u8* fb = gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL);
     
-    u32 x, y, pos;
-    for (x = 0; x < 400; x++) {
-        for (y = 0; y < 720; y += 3) {
-            pos = x * 720 + y;
-            fb[pos] = 0; // blue
-            fb[pos + 1] = 0; // green
-            fb[pos + 2] = 255; // red
-        }
-    }
+    fillScreen(fb, red);
     
     // Main loop
     while (aptMainLoop())
@@ -61,4 +57,53 @@ int main(int argc, char **argv)
     
     gfxExit();
     return 0;
+}
+
+void fillScreen(u8* fb, int color) {
+    int redValue, greenValue, blueValue;
+    switch(color) {
+        case red :
+            redValue = 255;
+            greenValue = blueValue = 0;
+            break;
+        case green :
+            redValue = blueValue = 0;
+            greenValue = 255;
+            break;
+        case blue :
+            redValue = greenValue = 0;
+            blueValue = 255;
+            break;
+        case yellow :
+            redValue = greenValue = 255;
+            blueValue = 0;
+            break;
+        case turquoise :
+            redValue = 0;
+            greenValue = blueValue = 255;
+            break;
+        case magenta :
+            redValue = blueValue = 255;
+            greenValue = 0;
+            break;
+        case white :
+            redValue = greenValue = blueValue = 255;
+            break;
+        case black :
+            redValue = greenValue = blueValue = 0;
+            break;
+        default :
+            redValue = greenValue = blueValue = 0;
+            break;
+    }
+    
+    u32 x, y, pos;
+    for (x = 0; x < 400; x++) {
+        for (y = 0; y < 720; y += 3) {
+            pos = x * 720 + y;
+            fb[pos] = blueValue; // blue
+            fb[pos + 1] = greenValue; // green
+            fb[pos + 2] = redValue; // red
+        }
+    }
 }
