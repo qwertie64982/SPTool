05689d2d697ab55319c3284e542b732d80b7f44b
Qwertie64982 <maxwell.a.sherman@live.com>
2018-09-26 20:25:15 -0700

    Add color picker and screen flashing option

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Stuck pixel tool for Nintendo 3DS
 Diagnose and potentially fix stuck pixels on either screen of the Nintendo 3DS. 
 
 ## Todo
-- Add colors other than red
-- Create separate function for filling a screen
-- Add UI
+- Add icon and description to makefile
+- Strange glitching on left of top screen when updating the frame
+- Add UI option to flip screens
--- source/main.c
+++ source/main.c
@@ -1,14 +1,15 @@
 /*
 SPTool by Maxwell Sherman (Qwertie64982)
-Last modified 2018-09-24 5PM PDT
+Last modified 2018-09-26 9PM PDT
 */
 
 #include <3ds.h>
 #include <stdio.h>
 
-enum {red, green, blue, yellow, turquoise, magenta, white, black};
+enum {red, green, blue, yellow, cyan, magenta, white, black};
 
 void fillScreen(u8* fb, int color);
+void setColorText(int color);
 
 int main(int argc, char **argv)
 {
@@ -21,33 +22,78 @@ int main(int argc, char **argv)
     consoleSelect(&bottomScreen);
     
     // row, col starting at 1
-    printf("\x1b[10;14HColor: ");
+    printf("\x1b[8;13HInstructions:");
+    printf("\x1b[10;13HColor (L/R): ");
+    printf("\x1b[12;13HFlash screen (A)");
     
-    printf("\x1b[10;21H\x1b[31mRed");
-    
-    //consoleSelect(&topScreen);
-    //
-    // Move cursor to 1,1
-    // printf("\x1b[1;1H");
-    // printf("\x1b[41m                                                  \n");
-    
-    printf("\x1b[30;11HPress Start to exit.");
+    printf("\x1b[28;12H\x1b[33mEpilepsy warning!!");
+    printf("\x1b[29;11H\x1b[37mPress Start to exit.");
+    printf("\x1b[30;4H\x1b[36mby Maxwell Sherman (Qwertie64982)");
     
     gfxSetDoubleBuffering(GFX_TOP, false);
     u8* fb = gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL);
     
-    fillScreen(fb, red);
+    int currentColor = red;
+    int newColor = red;
+    fillScreen(fb, currentColor); // initial screen is red
+    setColorText(currentColor);
+    
+    u32 keysPressed;
+    u32 keysReleased;
+    u32 keysHeldDown;
     
     // Main loop
     while (aptMainLoop())
     {
+        // Scan input
         hidScanInput();
         
-        u32 kDown = hidKeysDown();
+        // Store button states in u32s
+        keysPressed = hidKeysDown();
+        keysReleased = hidKeysUp();
+        keysHeldDown = hidKeysHeld();
         
-        if (kDown & KEY_START) {
+        // If Start pressed, exit the UI loop
+        if (keysPressed & KEY_START) {
             break;
         }
+        
+        // If D-Pad right, increment currentColor
+        if (keysPressed & KEY_DRIGHT) {
+            newColor++;
+        }
+        
+        // If D-Pad left, decrement currentColor
+        if (keysPressed & KEY_DLEFT) {
+            newColor--;
+        }
+        
+        // Validate and set new color
+        if (newColor != currentColor && !(keysHeldDown & KEY_A)) { // Only set newColor if it's different
+            if (newColor > black) {
+                newColor = red;
+            } else if (newColor < red) {
+                newColor = black;
+            }
+            currentColor = newColor;
+            fillScreen(fb, currentColor);
+            setColorText(currentColor);
+        }
+        
+        // If A, flash the screen
+        if (keysHeldDown & KEY_A) {
+            currentColor++;
+            if (currentColor > black) {
+                currentColor = red;
+            }
+            fillScreen(fb, currentColor);
+        }
+        /*
+        if (keysReleased & KEY_A) {
+            newColor = currentColor;
+            fillScreen(fb, currentColor);
+        }
+        */
         
         gfxFlushBuffers();
         gfxSwapBuffers();
@@ -59,7 +105,7 @@ int main(int argc, char **argv)
     return 0;
 }
 
-void fillScreen(u8* fb, int color) {
+void fillScreen(u8* fb, int color) { // TODO: There are artifacts - is this a framebuffer issue?
     int redValue, greenValue, blueValue;
     switch(color) {
         case red :
@@ -78,7 +124,7 @@ void fillScreen(u8* fb, int color) {
             redValue = greenValue = 255;
             blueValue = 0;
             break;
-        case turquoise :
+        case cyan :
             redValue = 0;
             greenValue = blueValue = 255;
             break;
@@ -106,4 +152,38 @@ void fillScreen(u8* fb, int color) {
             fb[pos + 2] = redValue; // red
         }
     }
+}
+
+void setColorText(int color) {
+    printf("\x1b[10;26H\x1b[40m"); // Move cursor to proper location and set BG to black
+    switch(color) {
+        case red :
+            printf("\x1b[31mRed    ");
+            break;
+        case green :
+            printf("\x1b[32mGreen  ");
+            break;
+        case blue :
+            printf("\x1b[34mBlue   ");
+            break;
+        case yellow :
+            printf("\x1b[33mYellow ");
+            break;
+        case cyan :
+            printf("\x1b[36mCyan   ");
+            break;
+        case magenta :
+            printf("\x1b[35mMagenta");
+            break;
+        case white :
+            printf("\x1b[37mWhite  ");
+            break;
+        case black :
+            printf("\x1b[30;47mBlack\x1b[40m  "); // black text on white BG
+            break;
+        default :
+            printf("\x1b[30;47mBlack\x1b[40m  ");
+            break;
+    }
+    printf("\x1b[37m\x1b[46m"); // Reset text FG/BG color to white/black respectively
 }
